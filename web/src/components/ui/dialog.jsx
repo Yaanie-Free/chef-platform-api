071f55a2a3b2ba29a263adabb1c7dd2eabf8c37f
Yaanie-Free <daiyaanf@tfg.co.za>
2025-10-18 21:01:10 +0000

    commit

---
+++ web/src/components/ui/dialog.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import * as DialogPrimitive from '@radix-ui/react-dialog'
+
+const Dialog = DialogPrimitive.Root
+const DialogTrigger = DialogPrimitive.Trigger
+const DialogPortal = DialogPrimitive.Portal
+const DialogClose = DialogPrimitive.Close
+
+const DialogOverlay = React.forwardRef(({ className, ...props }, ref) => (
+  <DialogPrimitive.Overlay
+    ref={ref}
+    className="fixed inset-0 z-50 bg-black/50 backdrop-blur-sm"
+    {...props}
+  />
+))
+DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
+
+const DialogContent = React.forwardRef(({ className, children, ...props }, ref) => (
+  <DialogPortal>
+    <DialogOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className="fixed left-[50%] top-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white p-6 shadow-lg"
+      {...props}
+    >
+      {children}
+    </DialogPrimitive.Content>
+  </DialogPortal>
+))
+DialogContent.displayName = DialogPrimitive.Content.displayName
+
+export { Dialog, DialogTrigger, DialogContent, DialogClose }
